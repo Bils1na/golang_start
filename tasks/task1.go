41b21a85f47b2aa8520e13c7b251901b34bbb400
bilsina <bils1n@icloud.com>
2023-07-04 22:17:28 +0300

    task1

--- tasks/task1.go
+++ tasks/task1.go
@@ -1,3 +1,15 @@
+// Дано трехзначное число. Найдите сумму его цифр.
+
+// Формат входных данных
+// На вход дается трехзначное число.
+
+// Формат выходных данных
+// Выведите одно целое число - сумму цифр введенного числа.
+
+// Sample Input:
+// 745
+// Sample Output:
+// 16
 package main
 
 import (
@@ -5,6 +17,14 @@ import (
 )
 
 func main() {
-	var a string = "A"
-	fmt.Print(a)
+	var number int
+	fmt.Scan(&number)
+	var sum int
+
+	for number > 0 {
+		sum += number % 10
+		number /= 10
+	}
+
+	fmt.Println(sum)
 }
